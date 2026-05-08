2392042729e2b80a0e34e663655be059795589af
Geor Guzitaev <georguzitaev@gmail.com>
2026-05-08 17:13:07 +0300

    Add project files.

---
+++ SDA.Db/AppContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SDA.Db
+{
+    public class AppContext: DbContext
+    {
+
+
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=database.db");
+        }
+
+
+    }
+}
---
+++ SDA.Web/Controllers/ExamController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SDAProject.Controllers
+{
+    public class ExamController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+    }
+}
