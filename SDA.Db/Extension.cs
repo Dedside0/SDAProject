43dde5e6b580f97eccb6f9d0fae98672189fc69e
Geor Guzitaev <georguzitaev@gmail.com>
2026-05-08 18:51:10 +0300

    добавил репозитории и класс расширения для внедрения зависимостей

---
+++ SDA.Db/Extension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SDA.Db.Repositories;
+
+namespace SDA.Db
+{
+    public static class Extension
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<ITicketRepository, TicketRepository>();
+            services.AddScoped<IQuestionRepository,QuestionRepository>();
+            return services;
+        }
+
+        public static IServiceCollection AddDataBase(this IServiceCollection services)
+        {
+            services.AddDbContext<AppContext>();
+            return services;
+        }
+    }
+}
--- SDA.Web/Program.cs
+++ SDA.Web/Program.cs
@@ -1,3 +1,4 @@
+using SDA.Db;
 namespace SDA.Web
 {
     public class Program
@@ -6,16 +7,18 @@ namespace SDA.Web
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+           
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddRepositories();
+            builder.Services.AddDataBase();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
