5751b27be7b08dfde3e58f629dff835a2ea5a894
Geor Guzitaev <georguzitaev@gmail.com>
2026-05-27 16:14:11 +0300

    сделал вьюшку для регистрации

---
+++ SDA.Web/Controllers/AuthorizationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SDA.Web.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
